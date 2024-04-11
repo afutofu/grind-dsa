8704e34b9bcc54bcdfa715220c642adeb27e9bf0
Muhammad Afuzarahman <theafuza@gmail.com>
2024-04-11 00:15:35 -0700

    Remove unncessary steps for Ransom Note

--- week-1/15 Ransom Note/ransom_note.py
+++ week-1/15 Ransom Note/ransom_note.py
@@ -4,15 +4,11 @@ class Solution:
         if len(ransomNote) > len(magazine):
             return False
 
-        # Convert strings to character lists
-        magList = [char for char in magazine]
-        ranList = [char for char in ransomNote]
-
         # Make alphabet counter using an array of 26 indexes for each letter
         alphabet_counter = [0] * 26
 
         # Add up counter from magazine
-        for c in magList:
+        for c in magazine:
             index = ord(c) - ord("a")
             alphabet_counter[index] += 1
 
