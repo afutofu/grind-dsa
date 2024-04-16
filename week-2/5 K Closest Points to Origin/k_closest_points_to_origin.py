776a7313f5999fbca940dcb6ff29d54730cb5ca3
Muhammad Afuzarahman <theafuza@gmail.com>
2024-04-15 23:21:58 -0700

    Add code for K Closest Points to Origin

---
+++ week-2/5 K Closest Points to Origin/k_closest_points_to_origin.py
@@ -0,0 +1,17 @@
+import heapq
+from typing import List
+
+
+class Solution:
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+
+        for x, y in points:
+            dist = -(x * x + y * y)
+            if len(heap) == k:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+
+        return [(x, y) for (dist, x, y) in heap]
