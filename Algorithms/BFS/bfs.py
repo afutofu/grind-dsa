6f3f43a5081836f69d2a9b657f7e77250686951c
Muhammad Afuzarahman <theafuza@gmail.com>
2024-05-03 01:52:19 -0700

    Reimplement BFS algorithm uisng python queue

--- Algorithms/BFS/bfs.py
+++ Algorithms/BFS/bfs.py
@@ -1,26 +1,22 @@
-import sys
-
-# caution: path[0] is reserved for script path (or '' in REPL)
-sys.path.insert(1, "../Queue")
-
-from queue import Queue
+# Use python queue module to implement a queue
+import queue
 
 
 def bfs(graph, start):
     # Initialize a set to keep track of visited nodes
     # Initialize a queue to keep track of nodes to visit
     visited = set()
-    found = Queue()
+    found = queue.Queue()
 
     # Add the starting node to the queue and mark it as visited
-    found.enqueue(start)
+    found.put(start)
     visited.add(start)
 
     # While there are nodes to visit
-    while not found.is_empty():
+    while not found.empty():
 
         # Get the current node from the queue
-        current = found.dequeue()
+        current = found.get()
 
         # Print the current node
         print(current, end=" ")
@@ -31,7 +27,7 @@ def bfs(graph, start):
             # If the neighbor has not been visited, mark it as visited and add it to the queue
             if neighbor not in visited:
                 visited.add(neighbor)
-                found.enqueue(neighbor)
+                found.put(neighbor)
 
 
 if __name__ == "__main__":
